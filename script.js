4809e2a1fc881d1b9dcd37fb10b2d2cfea1fe331
Matheus Antonio <dev.matheus.antonio@outlook.com>
2023-06-04 16:41:57 -0300

    pages

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CARROSSEL ANIMADO
 let contador = 1;
 
 setInterval(function(){
@@ -8,3 +9,20 @@ setInterval(function(){
     contador = '1';
   }
 }, 3000);
+
+// MENU MOBILE
+function menuShow(){
+  let menuMobile = document.querySelector('.mobile-menu');
+  let iconBar = document.querySelector('.icon-bar');
+  let iconX = document.querySelector('.icon-x');
+
+  if (menuMobile.classList.contains('open')){
+    menuMobile.classList.remove('open');
+    iconX.classList.remove('show');
+    iconBar.classList.add('show');
+  } else {
+    menuMobile.classList.add('open');
+    iconBar.classList.remove('show');
+    iconX.classList.add('show');
+  }
+}
