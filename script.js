1bf49c3a04efcf651cec150e1bba67ae2f95e693
Matheus Antonio <dev.matheus.antonio@outlook.com>
2023-06-05 15:58:27 -0300

    scroll

--- script.js
+++ script.js
@@ -28,6 +28,31 @@ function menuShow(){
   }
 }
 
+// MOVIMENTAÇÃO DO SCROLL
+const menuItens = document.querySelectorAll('.menu-img a[href^="#"]');
+
+function getDistanceFromTheTop(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+function nativeScroll(distanceFromTheTop) {
+  window.scroll({
+    top: distanceFromTheTop,
+    behavior: "smooth",
+  });
+}
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const distanceFromTheTop = getDistanceFromTheTop(event.target);
+  nativeScroll(distanceFromTheTop);
+}
+
+menuItens.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+})
+
 // BOTÃO SUBIR
 const btn = document.getElementById("btnTop");
 
@@ -37,3 +62,15 @@ btn.addEventListener("click", function(){
     behavior: "smooth",
   })
 })
+
+document.addEventListener('scroll', hide)
+
+function hide(){
+  if(window.scrollY > 10){
+    btn.style.display = "flex"
+  } else {
+    btn.style.display = "none"
+  }
+}
+
+hide();
